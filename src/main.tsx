665938a534d5ece5898851860f741e9c0bdd85e2
namdaeun <nde40345@gmail.com>
2024-12-30 01:24:33 +0900

    init: vite & yarn-berry setting

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+
+const element = document.getElementById("root");
+
+if (!element) {
+  throw new Error("Root element not found.");
+}
+
+const root = createRoot(element);
+root.render(<App />);
