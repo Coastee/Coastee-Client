3889b860dd90d8908b09d73692aac11024ac00c0
namdaeun <nde40345@gmail.com>
2025-01-04 02:55:37 +0900

    #1 [INIT] tanstack-query 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
+
+const queryClient = new QueryClient();
 
 const element = document.getElementById("root");
 
@@ -9,4 +13,11 @@ if (!element) {
 }
 
 const root = createRoot(element);
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
