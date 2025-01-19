c4a43bc1551489cd1e5fc67087703544c9b28afa
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-01-19 16:02:51 +0900

    [CHORE] netlify 배포 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { router } from "@/routers/Router";
-import { RouterProvider } from "react-router-dom";
+// import { router } from "@/routers/Router";
+// import { RouterProvider } from "react-router-dom";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  // return <RouterProvider router={router} />;
+  return <div>Hello World!</div>;
 };
 
 export default App;
