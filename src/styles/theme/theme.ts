1e8131ff205d1c80e5629236ff316f65cb45b439
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-01-20 17:34:16 +0900

    #4 [FEAT] theme.ts에 colors 셋팅

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,1 +1,29 @@
-export const theme = {};
+export const theme = {
+  colors: {
+    // White
+    white: "#ffffff",
+    white1: "#f6f6f6",
+    white2: "#f0f0f0",
+
+    // Gray
+    gray0: "#dfdfdf",
+    gray1: "#cacaca",
+    gray2: "#b9b9b9",
+    gray3: "#969696",
+    gray4: "#666666",
+
+    // Dark-Blue
+    darkBlue0: "#2b3340",
+    darkBlue1: "#1d2229",
+
+    // Black
+    black: "#000000",
+
+    // Primary
+    primaryBlue0: "#9fb3ff",
+    primaryBlue1: "#6b8afd",
+    primaryBlue2: "#3d66ff",
+    primaryPink0: "#ff9fbc",
+    primaryPink1: "#ff6593",
+  },
+};
