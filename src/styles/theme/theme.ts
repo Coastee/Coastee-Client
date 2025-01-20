3a7ed2055f2d4a14a6d33fc62ad5da92ea5f0ad7
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-01-20 18:41:32 +0900

    #4 [FEAT] theme.ts에 fontSize 셋팅

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,29 +1,39 @@
+const color = {
+  // White
+  white: "#ffffff",
+  white1: "#f6f6f6",
+  white2: "#f0f0f0",
+
+  // Gray
+  gray0: "#dfdfdf",
+  gray1: "#cacaca",
+  gray2: "#b9b9b9",
+  gray3: "#969696",
+  gray4: "#666666",
+
+  // Dark-Blue
+  darkBlue0: "#2b3340",
+  darkBlue1: "#1d2229",
+
+  // Black
+  black: "#000000",
+
+  // Primary
+  primaryBlue0: "#9fb3ff",
+  primaryBlue1: "#6b8afd",
+  primaryBlue2: "#3d66ff",
+  primaryPink0: "#ff9fbc",
+  primaryPink1: "#ff6593",
+};
+
+const fontSize = {
+  title: "32px",
+  body1: "16px",
+  body2: "14px",
+  body3: "12px",
+};
+
 export const theme = {
-  colors: {
-    // White
-    white: "#ffffff",
-    white1: "#f6f6f6",
-    white2: "#f0f0f0",
-
-    // Gray
-    gray0: "#dfdfdf",
-    gray1: "#cacaca",
-    gray2: "#b9b9b9",
-    gray3: "#969696",
-    gray4: "#666666",
-
-    // Dark-Blue
-    darkBlue0: "#2b3340",
-    darkBlue1: "#1d2229",
-
-    // Black
-    black: "#000000",
-
-    // Primary
-    primaryBlue0: "#9fb3ff",
-    primaryBlue1: "#6b8afd",
-    primaryBlue2: "#3d66ff",
-    primaryPink0: "#ff9fbc",
-    primaryPink1: "#ff6593",
-  },
+  color,
+  fontSize,
 };
