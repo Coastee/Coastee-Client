88fd48e49e5e4555bec6921b5968cb3bab4f1e82
namdaeun <nde40345@gmail.com>
2025-01-21 23:17:18 +0900

    #11 [STYLE] 아이콘 배치

--- src/components/Input/Input.styles.ts
+++ src/components/Input/Input.styles.ts
@@ -1,27 +1,48 @@
 import { theme } from "@/styles/theme/theme";
 import { css } from "@emotion/react";
 
-export const inputStyle = css`
+export const wrapperStyle = (
+  hasLeftIcon: boolean,
+  hasRightIcon: boolean,
+  isError: boolean
+) => css`
+  display: flex;
+
   width: 35rem;
   height: 5rem;
 
   padding: 1.55rem 1.8rem;
 
-  border-radius: 10px;
-  border: 1px solid ${theme.color.dark1};
+  gap: ${hasLeftIcon && "1.35rem"};
+  justify-content: ${hasRightIcon && "space-between"};
+  align-items: center;
+  flex-shrink: 0;
 
-  color: ${theme.color.gray1};
+  border-radius: 10px;
+  box-shadow: inset 0 0 0 1px
+    ${isError ? theme.color.primaryPink1 : theme.color.dark1};
+
   background-color: #2d3643;
 
+  transition: 0.2s ease-in-out;
+
+  :focus-within {
+    box-shadow: inset 0 0 0 1px ${theme.color.primaryBlue3};
+  }
+`;
+
+export const inputStyle = () => css`
+  width: 35rem;
+
+  border: none;
+
+  color: ${theme.color.gray1};
+  background-color: transparent;
+
   outline: none;
+  transition: 0.2s ease-in-out;
 
   ::placeholder {
     ${theme.font.body1};
-  }
-
-  :focus {
-    border: 1px solid ${theme.color.primaryBlue3};
-
-    transition: 0.2s ease-in-out;
   }
 `;
--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,19 +1,36 @@
-import { inputStyle } from "@/components/Input/Input.styles";
-import type { InputHTMLAttributes } from "react";
+import { inputStyle, wrapperStyle } from "@/components/Input/Input.styles";
+import type { InputHTMLAttributes, ReactNode } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  variant?: "primary" | "count";
+  variant?: "input" | "textarea";
   isError?: boolean;
+  leftIcon?: ReactNode;
+  rightIcon?: ReactNode;
 }
 
 const Input = ({
-  variant = "primary",
+  variant = "input",
   isError = false,
+  leftIcon,
+  rightIcon,
   placeholder,
-  maxLength,
+  id,
   value,
+  ...props
 }: InputProps) => {
-  return <input css={inputStyle} placeholder={placeholder} value={value} />;
+  return (
+    <div css={wrapperStyle(!!leftIcon, !!rightIcon, isError)}>
+      {leftIcon}
+      <input
+        css={inputStyle}
+        placeholder={placeholder}
+        id={id}
+        value={value}
+        {...props}
+      />
+      <div css={{ cursor: "pointer" }}>{rightIcon}</div>
+    </div>
+  );
 };
 
 export default Input;
