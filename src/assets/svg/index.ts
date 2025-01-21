d0d0967ac954f39e2148d5147f775c9e328bd280
namdaeun <nde40345@gmail.com>
2025-01-22 00:33:16 +0900

    #11 [CHORE] 아이콘 import

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,4 +1,5 @@
 export { default as Logo1Icon } from "./Logo1Icon";
 export { default as Logo2Icon } from "./Logo2Icon";
 export { default as Logo3Icon } from "./Logo3Icon";
+export { default as RotateLogoIcon } from "./RotateLogoIcon";
 export { default as Vite } from "./Vite";
