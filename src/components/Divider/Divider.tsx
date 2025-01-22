286956d9f1438239b86aa5392a6d191630a8272a
namdaeun <nde40345@gmail.com>
2025-01-22 16:04:26 +0900

    #13 [FEAT] props 전달

--- src/components/Divider/Divider.tsx
+++ src/components/Divider/Divider.tsx
@@ -6,9 +6,9 @@ interface DividerProps extends HTMLAttributes<HTMLHRElement> {
   variant?: "single" | "date";
 }
 
-const Divider = ({ variant = "single", children }: DividerProps) => {
+const Divider = ({ variant = "single", children, ...props }: DividerProps) => {
   return (
-    <div css={s.wrapperStyle(variant)}>
+    <div css={s.wrapperStyle(variant)} {...props}>
       <hr css={s.dividerStyle} />
       {variant === "date" && <p css={s.childrenStyle}>{children}</p>}
       <hr css={s.dividerStyle} />
