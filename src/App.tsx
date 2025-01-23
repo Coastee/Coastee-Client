26b1a15b0c5cebd7f3e8946c03219191c03cf89d
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-01-23 23:56:36 +0900

    #10 [FIX] HomePage에 작성한 내용 안 뜨는 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { MenuHeader, ServerHeader } from "@/components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
   return (
     <div css={{ display: "flex" }}>
       <ServerHeader />
       <MenuHeader />
+      <Outlet />
     </div>
   );
 };
