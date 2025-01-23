c336cf2ed549386bdcb64ca541bb84995fbdcc5f
namdaeun <nde40345@gmail.com>
2025-01-23 20:15:22 +0900

    #17 [FEAT] 온보딩 페이지 구현

---
+++ src/pages/OnboardingPage/OnboardingPage.styles.ts
@@ -0,0 +1,39 @@
+import { theme } from "@/styles/theme/theme";
+import { css } from "@emotion/react";
+
+export const commonBtnStyle = css`
+  display: flex;
+
+  width: 100%;
+
+  padding: 1.4rem 9.8rem;
+
+  align-items: center;
+  justify-content: center;
+  gap: 0.95rem;
+
+  border: none;
+  border-radius: 1rem;
+
+  font-weight: 500;
+  ${theme.font.body1};
+
+  cursor: pointer;
+`;
+
+export const googleBtnStyle = css`
+  border: 1px solid ${theme.color.dark1};
+
+  color: ${theme.color.white};
+  background-color: transparent;
+`;
+
+export const kakaoBtnStyle = css`
+  color: ${theme.color.dark3};
+  background-color: ${theme.color.yellow};
+`;
+
+export const naverBtnStyle = css`
+  color: ${theme.color.white};
+  background-color: ${theme.color.green};
+`;
---
+++ src/pages/OnboardingPage/OnboardingPage.tsx
@@ -0,0 +1,35 @@
+import { GoogleIcon, KakaoIcon, Logo4WhiteIcon, NaverIcon } from "@/assets/svg";
+import AuthContainer from "@/components/AuthContainer/AuthContainer";
+import * as s from "@/pages/OnboardingPage/OnboardingPage.styles";
+
+const OnBoardingPage = () => {
+  return (
+    <>
+      <AuthContainer
+        title="시작하기"
+        desc={
+          <>
+            Welcome to <Logo4WhiteIcon width={90} height={30} />
+          </>
+        }
+      >
+        <div css={{ display: "flex", flexDirection: "column", gap: "1.8rem" }}>
+          <button type="button" aria-label="Google로 시작하기" css={[s.commonBtnStyle, s.googleBtnStyle]}>
+            <GoogleIcon width={22} height={22} />
+            Google로 시작하기
+          </button>
+          <button type="button" aria-label="카카오로 시작하기" css={[s.commonBtnStyle, s.kakaoBtnStyle]}>
+            <KakaoIcon width={21} height={19} />
+            카카오로 시작하기
+          </button>
+          <button type="button" aria-label="네이버로 시작하기" css={[s.commonBtnStyle, s.naverBtnStyle]}>
+            <NaverIcon width={19} height={18} />
+            네이버로 시작하기
+          </button>
+        </div>
+      </AuthContainer>
+    </>
+  );
+};
+
+export default OnBoardingPage;
