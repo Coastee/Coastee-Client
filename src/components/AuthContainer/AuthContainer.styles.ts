e632a5b71973f2e464fc954b27baf524dba891fd
namdaeun <nde40345@gmail.com>
2025-01-23 20:15:57 +0900

    #17 [FEAT] 배경 이미지 애니메이션 추가

--- src/components/AuthContainer/AuthContainer.styles.ts
+++ src/components/AuthContainer/AuthContainer.styles.ts
@@ -1,10 +1,93 @@
 import { theme } from "@/styles/theme/theme";
-import { css } from "@emotion/react";
+import { css, keyframes } from "@emotion/react";
+
+const fallDownAnimation = keyframes`
+  0% {
+    transform: translateY(-50%);
+    opacity: 0.3;
+  }
+  100% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+`;
+
+const floatAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-15px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+export const logo1Style = css`
+  position: absolute;
+
+  top: 15%;
+  left: 0%;
+
+  width: 73.8rem;
+  height: 63.7rem;
+
+   animation:
+    ${fallDownAnimation} 2.5s ease-in-out forwards,
+    ${floatAnimation} 2s ease-in-out infinite 5s;
+
+  transform-origin: center;
+  will-change: transform;
+`;
+
+export const logo2Style = css`
+  position: absolute;
+
+  top: 0%;
+  right: 0%;
+
+  width: 61.6rem;
+  height: 72.8rem;
+
+  animation: ${fallDownAnimation} 5s ease-in-out forwards,
+  ${floatAnimation} 3s ease-in-out infinite 5s;
+
+  transform-origin: center;
+  will-change: transform;
+
+`;
+
+export const wrapperStyle = css`
+  display: flex;
+
+  width: 100%;
+  height: 100vh;
+
+  align-items: center;
+  justify-content: center;
+
+  position: relative;
+
+  overflow: hidden;
+`;
 
 export const sectionStyle = css`
   display: flex;
 
+  width: 81rem;
+  height: 38rem;
+
   padding: 5rem 7.8rem 5rem 6rem;
+
+  justify-content: space-between;
+  gap: 1.6rem;
+
+  border-radius: 2.5rem;
+
+  background-color: ${theme.color.dark3};
+
+  z-index: 1;
 `;
 
 export const titleStyle = css`
@@ -17,8 +100,14 @@ export const titleStyle = css`
 `;
 
 export const descStyle = css`
+  display: flex;
+  align-items: center;
+
+  gap: 0.3rem;
+
   color: ${theme.color.white2};
 
+  font-weight: 400;
   ${theme.font.body1};
 `;
 
@@ -26,14 +115,14 @@ export const childrenStyle = css`
   display: flex;
 
   flex-direction: column;
-
   gap: '1.8rem';
+
+  padding-top: 7.1rem;
 `;
 
 export const sideStyle = css`
   display: flex;
 
   flex-direction: column;
-
   gap: '2.1rem'
 `;
--- src/components/AuthContainer/AuthContainer.tsx
+++ src/components/AuthContainer/AuthContainer.tsx
@@ -1,23 +1,29 @@
-import { Logo2Icon } from "@/assets/svg";
+import logo1Img from "@/assets/img/logo1Img.png";
+import logo2Img from "@/assets/img/logo2Img.png";
+import { Logo2WhiteIcon } from "@/assets/svg";
 import * as s from "@/components/AuthContainer/AuthContainer.styles";
 import type { ReactNode } from "react";
 
 interface AuthContainerProps {
   title?: string;
-  desc?: string;
-  children: ReactNode;
+  desc?: ReactNode;
+  children?: ReactNode;
 }
 
 const AuthContainer = ({ title, desc, children }: AuthContainerProps) => {
   return (
-    <section css={s.sectionStyle}>
-      <div css={s.sideStyle}>
-        <Logo2Icon width={62} height={38} />
-        <h1 css={s.titleStyle}>{title}</h1>
-        <h2 css={s.descStyle}>{desc}</h2>
-      </div>
-      <div css={[s.childrenStyle]}>{children}</div>
-    </section>
+    <div css={s.wrapperStyle} className="background">
+      <img src={logo1Img} alt="로고1" css={s.logo1Style} />
+      <img src={logo2Img} alt="로고2" css={s.logo2Style} />
+      <section css={s.sectionStyle}>
+        <div css={s.sideStyle}>
+          <Logo2WhiteIcon width={62} height={38} css={{ marginBottom: "3.2rem" }} />
+          <h1 css={s.titleStyle}>{title}</h1>
+          <h2 css={s.descStyle}>{desc}</h2>
+        </div>
+        <div css={s.childrenStyle}>{children}</div>
+      </section>
+    </div>
   );
 };
 
