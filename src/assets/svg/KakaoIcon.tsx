e719f09cbce1a06732064608e4d138b935146d11
namdaeun <nde40345@gmail.com>
2025-01-23 20:14:50 +0900

    #17 [CHORE] 필요한 아이콘 및  색상 추가

---
+++ src/assets/svg/KakaoIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgKakaoIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 22 20"
+    {...props}
+  >
+    <path
+      fill="#3E3E3E"
+      fillRule="evenodd"
+      d="M10.75 16.21c5.826 0 10.55-3.464 10.55-7.736S16.575.74 10.75.74C4.926.739.204 4.202.204 8.474c0 2.61 1.76 4.916 4.459 6.318.007.003.003.014-.004.013a.01.01 0 0 0-.009.004l-1.263 4.347a.5.5 0 0 0 .748.562L9.57 16.26a.5.5 0 0 1 .304-.078q.435.027.877.027"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgKakaoIcon;
