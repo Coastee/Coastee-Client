737c4b9968de7c85b591cb4987eefb633869313d
namdaeun <nde40345@gmail.com>
2025-01-26 20:55:24 +0900

    #19 [CHORE] lib 추가 및 index 수정

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,8 @@
-export { default as ServerHeader } from "./ServerHeader/ServerHeader";
-export { default as MenuHeader } from "./MenuHeader/MenuHeader";
+export { default as AuthContainer } from "./AuthContainer/AuthContainer";
 export { default as Button } from "./Button/Button";
+export { default as CheckBox } from "./CheckBox/Checkbox";
+export { default as Divider } from "./Divider/Divider";
+export { default as Input } from "./Input/Input";
+export { default as MenuHeader } from "./MenuHeader/MenuHeader";
+export { default as ServerHeader } from "./ServerHeader/ServerHeader";
+export { default as Textarea } from "./Textarea/Textarea";
