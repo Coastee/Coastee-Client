496c3c3d6df388cdc2c39c7a40067f3a1358572c
namdaeun <nde40345@gmail.com>
2025-01-26 21:57:49 +0900

    #19 [FIX] useFunnel step 초기값 설정

--- src/hooks/useFunnel.tsx
+++ src/hooks/useFunnel.tsx
@@ -1,4 +1,4 @@
-import { type ReactElement, type ReactNode, isValidElement, useState } from "react";
+import { Children, type ReactElement, type ReactNode, isValidElement, useState } from "react";
 
 type FunnelStepProps = {
   name: string;
@@ -6,27 +6,27 @@ type FunnelStepProps = {
 };
 
 type FunnelComponent = {
-  (props: { children: ReactNode }): ReactElement | null;
-  Step: (props: FunnelStepProps) => ReactElement;
+  (props: { children: ReactNode }): ReactNode;
+  Step: (props: FunnelStepProps) => ReactNode;
   setStep: (step: string) => void;
 };
 
 export const useFunnel = () => {
-  const [step, setStep] = useState<string | null>(null);
+  const [step, setStep] = useState<string>("step1");
 
-  const Step = ({ name, children }: FunnelStepProps) => {
+  const Step = ({ children }: Omit<FunnelStepProps, "name">) => {
     return <>{children}</>;
   };
 
-  const Funnel = ({ children }: { children: ReactNode }): ReactElement | null => {
-    const stepsArray = Array.isArray(children) ? children : [children];
+  const Funnel = ({ children }: { children: ReactNode }): ReactNode => {
+    const steps = Children.toArray(children) as ReactElement<FunnelStepProps>[];
 
-    const targetStep = stepsArray.find(
+    const targetStep = steps.find(
       (child): child is ReactElement<FunnelStepProps> =>
         isValidElement<FunnelStepProps>(child) && child.props.name === step,
     );
 
-    return targetStep ?? null;
+    return targetStep;
   };
 
   return Object.assign(Funnel, { Step, setStep }) as FunnelComponent;
--- src/pages/SignupPage/SignupPage.tsx
+++ src/pages/SignupPage/SignupPage.tsx
@@ -3,18 +3,12 @@ import ContactStep from "@/pages/SignupPage/components/ContactStep/ContactStep";
 import InfoStep from "@/pages/SignupPage/components/InfoStep/InfoStep";
 import InterestStep from "@/pages/SignupPage/components/InterestStep/InterestStep";
 import ProfileStep from "@/pages/SignupPage/components/ProfileStep/ProfileStep";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const SignupPage = () => {
   const Funnel = useFunnel();
 
   const navigate = useNavigate();
-
-  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-  useEffect(() => {
-    Funnel.setStep("step1");
-  }, []);
 
   return (
     <Funnel>
