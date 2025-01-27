d546e6eb530b17299bff06cf9dfa7bdbbfebc40b
namdaeun <nde40345@gmail.com>
2025-01-27 18:56:43 +0900

    chore: 필요 없는 코드 제거

--- src/components/AuthContainer/AuthContainer.styles.ts
+++ src/components/AuthContainer/AuthContainer.styles.ts
@@ -1,55 +1,5 @@
 import { theme } from "@/styles/theme/theme";
-import { css, keyframes } from "@emotion/react";
-
-const fallDownAnimation = keyframes`
-  0% {
-    transform: translateY(-50%);
-    opacity: 0.3;
-  }
-  100% {
-    transform: translateY(0px);
-    opacity: 1;
-  }
-`;
-
-const floatAnimation = keyframes`
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-15px);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
-
-export const logo1Style = css`
-  position: absolute;
-
-  top: 15%;
-  left: 0%;
-
-  width: 73.8rem;
-  height: 63.7rem;
-
-   animation:
-    ${fallDownAnimation} 2.5s ease-in-out forwards,
-    ${floatAnimation} 2s ease-in-out infinite 3.8s;
-`;
-
-export const logo2Style = css`
-  position: absolute;
-
-  top: 0%;
-  right: 0%;
-
-  width: 61.6rem;
-  height: 72.8rem;
-
-  animation: ${fallDownAnimation} 3.5s ease-in-out forwards,
-  ${floatAnimation} 3s ease-in-out infinite 3.8s;
-`;
+import { css } from "@emotion/react";
 
 export const wrapperStyle = css`
   display: flex;
@@ -127,5 +77,5 @@ export const sideStyle = css`
 
   flex-direction: column;
   flex-grow: 1;
-  gap: 2.1rem
+  gap: 2.1rem;
 `;
