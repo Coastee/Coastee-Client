8a029a4760645005de4d68145de892af6a8d238e
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2025-01-30 17:08:23 +0900

    #26 [FIX] HomePage에 불필요한 Dropdown 제거

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,20 +1,13 @@
 import { GroupChatList, TitleContainer } from "@/components";
-import { SORTING_OPTIONS } from "@/constants/dropdown";
-import { useState } from "react";
 import * as s from "./HomePage.styles";
 
 export const HomePage = () => {
-  const [sortingOption, setSortingOption] = useState(SORTING_OPTIONS[0]);
-
   return (
     <div css={s.containerStyle}>
       <TitleContainer
         title="그룹 채팅방"
         textButton="전체보기"
         handleTextButtonClick={() => console.log("전체보기")}
-        sortingOptions={SORTING_OPTIONS}
-        sortingOption={sortingOption}
-        setSortingOption={setSortingOption}
       >
         <GroupChatList />
       </TitleContainer>
