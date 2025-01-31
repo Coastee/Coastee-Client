b39dc0d7a3f7f8530a417e15e95716893381a191
namdaeun <nde40345@gmail.com>
2025-01-31 23:32:33 +0900

    #25 [FEAT] 필요한 이미지 및 아이콘

---
+++ src/assets/svg/DmIcon.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgDmIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 14 13"
+    {...props}
+  >
+    <path
+      fill="#fff"
+      fillRule="evenodd"
+      d="M13.483.519H.891v8.457h12.592zM4.04 8.976v3.543l6.296-3.543z"
+      clipRule="evenodd"
+    />
+  </svg>
+);
+export default SvgDmIcon;
