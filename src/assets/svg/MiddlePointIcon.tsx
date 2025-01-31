96e98fc5383569e9b17490ede72e969fa96278ce
namdaeun <nde40345@gmail.com>
2025-02-01 02:09:44 +0900

    #31 [CHORE] 아이콘 및 더미 추가

---
+++ src/assets/svg/MiddlePointIcon.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import type { SVGProps } from "react";
+const SvgMiddlePointIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 4 4"
+    {...props}
+  >
+    <circle cx={2.33} cy={1.544} r={1.5} fill="#CACACA" />
+  </svg>
+);
+export default SvgMiddlePointIcon;
--- src/pages/MyPage/constants/dummy.ts
+++ src/pages/MyPage/constants/dummy.ts
@@ -1,5 +1,6 @@
 import profileBgImg from "@/assets/img/profileBgImg.png";
 import profileImg from "@/assets/img/profileImg.png";
+import recommendedProfileImg from "@/assets/img/recommendProfileImg.png";
 
 export const DUMMY_PROFILE = {
   backgroundImg: profileBgImg,
@@ -10,3 +11,86 @@ export const DUMMY_PROFILE = {
     "I specialize in building modern, user-friendly web applications using React and TypeScript. Always eager to learn and improve UX.I specialize in building modern, user-friendly web applications using React and TypeScript. Always eager to learn and improve UX.I specialize in building modern, user-friendly web applications using React and TypeScript. Always eager to learn and improve UX.",
   urls: ["https://github.com/johndoe", "https://linkedin.com/in/johndoe"],
 };
+
+export const PROFILE_LIST = [
+  {
+    id: 1,
+    img: recommendedProfileImg,
+    name: "김준희",
+    intro: "프론트엔드 개발자",
+    experience: 5,
+    role: "홈 프로텍터",
+  },
+  {
+    id: 2,
+    img: recommendedProfileImg,
+    name: "윤서연",
+    intro: "UX/UI 디자이너",
+    experience: 3,
+    role: "프로덕트 디자이너",
+  },
+  {
+    id: 3,
+    img: recommendedProfileImg,
+    name: "박동우",
+    intro: "백엔드 개발자",
+    experience: 7,
+    role: "서버 엔지니어",
+  },
+  {
+    id: 4,
+    img: recommendedProfileImg,
+    name: "이하준",
+    intro: "AI 엔지니어",
+    experience: 4,
+    role: "머신러닝 리서처",
+  },
+  {
+    id: 5,
+    img: recommendedProfileImg,
+    name: "서지현",
+    intro: "프로젝트 매니저",
+    experience: 6,
+    role: "프로덕트 매니저",
+  },
+  {
+    id: 6,
+    img: recommendedProfileImg,
+    name: "한예린",
+    intro: "iOS 개발자",
+    experience: 2,
+    role: "모바일 앱 개발자",
+  },
+  {
+    id: 7,
+    img: recommendedProfileImg,
+    name: "최태혁",
+    intro: "데이터 분석가",
+    experience: 5,
+    role: "BI 엔지니어",
+  },
+  {
+    id: 8,
+    img: recommendedProfileImg,
+    name: "민경민",
+    intro: "게임 클라이언트 개발자",
+    experience: 3,
+    role: "언리얼 엔지니어",
+  },
+  {
+    id: 9,
+    img: recommendedProfileImg,
+    name: "송은채",
+    intro: "풀스택 개발자",
+    experience: 6,
+    role: "웹서비스 개발자",
+  },
+  {
+    id: 10,
+    img: recommendedProfileImg,
+    name: "황우진",
+    intro: "보안 엔지니어",
+    experience: 8,
+    role: "시스템 보안 전문가",
+  },
+];
